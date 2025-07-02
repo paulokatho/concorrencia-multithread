dfe2367cf2087f0a6211a33aaa84bb8919134858
paulokatho <pkatho@gmail.com>
2025-07-02 20:20:39 +0100

    feat: Inicio do projeto

---
+++ src/main/java/br/com/katho/processamento_multithread/ProcessamentoMultithreadApplication.java
@@ -0,0 +1,13 @@
+package br.com.katho.processamento_multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoMultithreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoMultithreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/katho/processamento_multithread/ProcessamentoMultithreadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.katho.processamento_multithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoMultithreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
